501eb04edb823c9f84279669d468ce1cdcf225a5
Sergio Sopze <60981650+Sopze92@users.noreply.github.com>
2024-04-19 20:28:25 +0000

    project finished

--- src/models.py
+++ src/models.py
@@ -1,29 +1,54 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+"""
+    user (a basic user instance)
+"""
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(255), nullable=False)
+    password = Column(String(16), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+"""
+    group (item types, such as 'character', 'vehicle', 'film', etc...)
+"""
+class Group(Base):
+    __tablename__ = 'group'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(255), nullable=False)
+
+"""
+    item (item instance)
+    foreign keys : 
+        group (the group this item belongs to)
+"""
+class Item(Base):
+    __tablename__ = 'item'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False)
+    group_id = Column(Integer, ForeignKey('group.id')) # intentionally nullable
+    group = relationship(Group)
+
+"""
+    bookmark (user saved item)
+    foreign keys : 
+        user (the user this item belongs to)
+        item (the item this bookmark points to)
+"""
+class Bookmark(Base):
+    __tablename__ = 'bookmark'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    item_id = Column(Integer, ForeignKey('item.id'), nullable=False)
+    user = relationship(User)
+    item = relationship(Item)
 
     def to_dict(self):
         return {}
